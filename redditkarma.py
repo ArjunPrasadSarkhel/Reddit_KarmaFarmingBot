2399ac9c282c91cf3b61fd4853a0ab25877d2d6c
Arjun Prasad Sarkhel <49405291+ArjunPrasadSarkhel@users.noreply.github.com>
2020-08-31 03:58:34 -0600

    Update redditkarma.py

--- redditkarma.py
+++ redditkarma.py
@@ -2,19 +2,19 @@ import praw
 import time
 from random import choice
 
-reddit = praw.Reddit(user_agent='test-script',
-                  client_id='En5dWl9v8IhrQA',
-                  client_secret='m7GxwQGjBK8GIjHK4b5s1275Yi0',
-                  username='HeavyIncomeTax',
-                  password='maaaaice623')
+reddit = praw.Reddit(user_agent='',
+                  client_id='',
+                  client_secret='',
+                  username='',
+                  password='')
 
 while True:
     try:
       with open("lines.txt") as f:
             lines = [l.rstrip() for l in f]
 
-      for comment in reddit.subreddit("FreeKarma4U").stream.comments(skip_existing=True):
-        if comment.author.name != "HeavyIncomeTax":
+      for comment in reddit.subreddit("").stream.comments(skip_existing=True):
+        if comment.author.name != "":
             comment.reply(choice(lines))
             comment.upvote()
             reply = comment.reply
